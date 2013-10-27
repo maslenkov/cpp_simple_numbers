206fcd70df3de52da52d5e6fb864fde8feb0731e
maslenkov <maslenkov@i.ua>
2013-10-27 18:06:59 +0300

    initial commit
    add first version of simple numbers finder

---
+++ b.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+const int SZ = 100000;
+const int MAX = 10000000;
+
+int main() {
+    //медленный алгоритм...
+    int n, numbers[SZ], number;
+    int i, j, k, tmp = 0;
+    bool is_simple;
+    cout << "Hello, World!!!" << endl;
+    cin >> n;
+    if (n > 1) {
+        numbers[1] = 3;
+    }
+    if (n > 2) {
+        number = 5;
+        is_simple = true;
+        for (i = 2; i < n; i++) {
+            while (true) {
+                for (j = 1; j < i; j++ ) {
+/*алгоритм получился медленный, мне кажеться, что можно использовать "расстояния"
+т.е. не проверять делиться ли число на 7, пока не будет достигнуто число 14...
+т.е. возможно вести два отдельных массива - один - для результат, второй для тех
+чисел на которые нужно проверять, что оно делиться...*/                    
+                    if (number % numbers[j] == 0) {
+                        is_simple = false;
+                        break;
+                    }
+                }
+                number += 2;
+                if (is_simple) {
+                    numbers[i] = number - 2;
+                    break;
+                } else {
+                    is_simple = true;
+                }
+                if (number > MAX) {
+                    i = i - 1;
+                    cout << "max number is: " << numbers[i] << endl;
+                    cout << "max index is: " << i << endl;
+                    return 0;
+                }
+            }
+        }
+    }
+    numbers[0] = 2;
+    for (i = (n > 9 ? n - 10 : 1); i < n; i++) {
+        cout << numbers[i] << ' ';
+    }
+    cout << endl;
+    cin >> i;
+    return 0;
+}
